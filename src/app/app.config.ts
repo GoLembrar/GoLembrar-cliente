402bb709b33a9f078e35e34917f112e6c966b611
jottaveDev <jviktor204@gmail.com>
2023-12-12 16:34:36 -0300

    chore: setup primeflex and primeng

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,12 @@
-import { ApplicationConfig } from '@angular/core'
-import { provideRouter } from '@angular/router'
-import { APP_ROUTES } from './app.routes'
-import { provideClientHydration } from '@angular/platform-browser'
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideRouter } from '@angular/router';
+import { APP_ROUTES } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideClientHydration(), provideRouter(APP_ROUTES)],
+  providers: [
+    provideClientHydration(), provideRouter(APP_ROUTES),
+    importProvidersFrom([BrowserModule, BrowserAnimationsModule])
+  ],
 }
