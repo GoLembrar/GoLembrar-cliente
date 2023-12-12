c8582d4b127a822cc37e23e3a8045a779fad3034
jottaveDev <jviktor204@gmail.com>
2023-12-12 17:02:45 -0300

    feat: create component register and setup routes

---
+++ src/app/core/pages/register/register.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
+
+@Component({
+  selector: 'gl-register',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.scss',
+})
+export class RegisterComponent {}
