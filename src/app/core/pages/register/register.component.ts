7df9b4aa9e2c0c9d48e9cfaa95cc62f245c7c470
jottaveDev <jviktor204@gmail.com>
2023-12-13 10:56:38 -0300

    feat: created and validated form

--- src/app/core/pages/register/register.component.ts
+++ src/app/core/pages/register/register.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core'
-import { CommonModule } from '@angular/common'
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators as V } from '@angular/forms';
 
 @Component({
   selector: 'gl-register',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  user = this.formBuilder.nonNullable.group({
+    email: [null, [V.required, V.email]],
+    password: [
+      null,
+      [V.required, V.pattern('^(?=.*?[A-Z])(?=.*?[a-z]).{6,}$')],
+    ],
+  })
+
+  onSubmit() {
+    if (this.user.valid) {
+      console.log(this.user.value);
+    } else {
+      console.log('Preencha um valor válido!');
+    }
+  }
+
+  constructor(private formBuilder: FormBuilder) {}
+}
