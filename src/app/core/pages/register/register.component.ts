e6fe9a54165f02a2e67ac9132c1f3d95d0f75c87
jottaveDev <jviktor204@gmail.com>
2023-12-13 15:27:37 -0300

    refactor: css components and html semantics

--- src/app/core/pages/register/register.component.ts
+++ src/app/core/pages/register/register.component.ts
@@ -1,28 +1,37 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule, Validators as V } from '@angular/forms';
-
+import { CommonModule } from '@angular/common'
+import { Component } from '@angular/core'
+import {
+  FormBuilder,
+  ReactiveFormsModule,
+  Validators as V,
+} from '@angular/forms'
+import { ButtonModule } from 'primeng/button'
+import { InputTextModule } from 'primeng/inputtext'
+import { PasswordModule } from 'primeng/password'
 @Component({
   selector: 'gl-register',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    PasswordModule,
+    ButtonModule,
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
   user = this.formBuilder.nonNullable.group({
-    email: [null, [V.required, V.email]],
-    password: [
-      null,
-      [V.required, V.pattern('^(?=.*?[A-Z])(?=.*?[a-z]).{6,}$')],
-    ],
+    email: ['', [V.required, V.email]],
+    password: ['', [V.required, V.pattern('^(?=.*?[A-Z])(?=.*?[a-z]).{6,}$')]],
   })
 
   onSubmit() {
     if (this.user.valid) {
-      console.log(this.user.value);
+      console.log(this.user.value)
     } else {
-      console.log('Preencha um valor válido!');
+      console.log('Preencha um valor válido!')
     }
   }
 
