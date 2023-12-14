438a9de800a347dacca3bd93e68a66f7d3c6a16f
jottaveDev <jviktor204@gmail.com>
2023-12-13 21:07:13 -0300

    feat: create and validate confirmPassword

--- src/app/core/pages/register/register.component.ts
+++ src/app/core/pages/register/register.component.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common'
 import { Component } from '@angular/core'
 import {
+  AbstractControl,
   FormBuilder,
   ReactiveFormsModule,
   Validators as V,
+  ValidatorFn
 } from '@angular/forms'
 import { ButtonModule } from 'primeng/button'
 import { InputTextModule } from 'primeng/inputtext'
@@ -22,17 +24,31 @@ import { PasswordModule } from 'primeng/password'
   styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
-  user = this.formBuilder.nonNullable.group({
+   user = this.formBuilder.group({
     email: ['', [V.required, V.email]],
     password: ['', [V.required, V.pattern('^(?=.*?[A-Z])(?=.*?[a-z]).{6,}$')]],
+    confirmPassword: ['', [V.required]],
+  }, {
+    validator: this.comparatePassword()
   })
 
-  onSubmit() {
-    if (this.user.valid) {
-      console.log(this.user.value)
-    } else {
-      console.log('Preencha um valor válido!')
-    }
+  comparatePassword(): ValidatorFn {
+    return (control: AbstractControl): {[key: string]: any} | null => {
+      const password = control.get('password');
+      const confirmPassword = control.get('confirmPassword');
+   
+      if (confirmPassword?.errors && !confirmPassword.errors['passwordMismatch']) {
+        return null;
+      }
+   
+      if (password && confirmPassword && password.value !== confirmPassword.value) {
+        confirmPassword.setErrors({ passwordMismatch: true });
+      } else {
+        confirmPassword?.setErrors(null);
+      }
+   
+      return null;
+    };
   }
 
   constructor(private formBuilder: FormBuilder) {}
