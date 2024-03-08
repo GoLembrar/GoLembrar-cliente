8bdeea4d32b98b2403cf6a6dde87b2b740094923
Andrew Domingues <drewdomi@proton.me>
2024-03-08 10:16:12 -0300

    feat: bottom bar & logout option

--- src/app/core/components/layout/layout.component.ts
+++ src/app/core/components/layout/layout.component.ts
@@ -3,12 +3,15 @@ import { CommonModule } from '@angular/common'
 import { RouterModule } from '@angular/router'
 import { AuthService } from '../../services/auth.service'
 
+import { SidebarModule } from 'primeng/sidebar'
+import { ButtonModule } from 'primeng/button'
+
 @Component({
   standalone: true,
   selector: 'gl-layout',
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, SidebarModule, ButtonModule],
 })
 export class LayoutComponent {
   constructor(private authService: AuthService) {}
