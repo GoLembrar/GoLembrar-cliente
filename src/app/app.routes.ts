08e2a0c499bf2beabea90ea91b046d1c8e4651e8
jottave <joao_victorflamengo10@outlook.com>
2024-04-10 17:27:19 -0300

    refactor: configure routes my contacts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,17 +4,24 @@ import { RegisterComponent } from './core/pages/register/register.component'
 import { HomeComponent } from './core/pages/home/home.component'
 import { LayoutComponent } from './core/components/layout/layout.component'
 import { authGuard } from './core/guards/auth/auth.guard'
-import { MyContactsComponent } from './core/pages/home/my-contacts/my-contacts.component'
 
 export const APP_ROUTES: Route[] = [
   {
     path: '',
     component: LayoutComponent,
     canActivate: [authGuard],
-    children: [{ path: '', component: HomeComponent }],
+    children: [
+      { path: '', component: HomeComponent },
+      {
+        path: 'my-contacts',
+        loadChildren: () =>
+          import('./core/pages/my-contacts/my-contacts.routes').then(
+            r => r.MY_CONTACTS_ROUTES
+          ),
+      },
+    ],
   },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
-  {path: 'my-contacts', component: MyContactsComponent},
   { path: '**', redirectTo: '' },
 ]
