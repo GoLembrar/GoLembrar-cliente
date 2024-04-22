aed7f17880771be25b7247acb2980b7c140de62a
jottave <joao_victorflamengo10@outlook.com>
2024-04-22 11:50:49 -0300

    feat: get contacts using mock

--- src/app/core/pages/my-contacts/my-contacts.component.ts
+++ src/app/core/pages/my-contacts/my-contacts.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
-import { Router, RouterModule } from '@angular/router'
+import { Component, OnInit } from '@angular/core'
+import { RouterModule } from '@angular/router'
+import { ButtonModule } from 'primeng/button'
+import { ChipModule } from 'primeng/chip'
+import { AddContactService } from '../../services/add-contact/add-contact.service'
 
 @Component({
   selector: 'gl-my-contacts',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, ChipModule, ButtonModule],
   templateUrl: './my-contacts.component.html',
   styleUrl: './my-contacts.component.scss',
 })
-export class MyContactsComponent {
-  constructor() {}
+export class MyContactsComponent implements OnInit {
+  constructor(private contact: AddContactService) {}
+
+  myContacts: any = []
+
+  ngOnInit(): void {
+    this.contact.getContacts().subscribe({
+      next: value => (this.myContacts = value),
+      error: err => console.log(err),
+    })
+  }
 }
