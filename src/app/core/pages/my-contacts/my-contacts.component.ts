99ffc9041a4e87ea6c4bbedf81f6fe9b076ab4c4
jottave <joao_victorflamengo10@outlook.com>
2024-04-30 11:08:49 -0300

    feat: configure routing to edit the contact

--- src/app/core/pages/my-contacts/my-contacts.component.ts
+++ src/app/core/pages/my-contacts/my-contacts.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common'
 import { Component, OnInit } from '@angular/core'
-import { RouterModule } from '@angular/router'
+import { Router, RouterModule } from '@angular/router'
 import { ButtonModule } from 'primeng/button'
 import { ChipModule } from 'primeng/chip'
 import { Contact } from '../../models/contact'
@@ -14,14 +14,23 @@ import { AddContactService } from '../../services/add-contact/add-contact.servic
   styleUrl: './my-contacts.component.scss',
 })
 export class MyContactsComponent implements OnInit {
-  constructor(private contact: AddContactService) {}
+  constructor(
+    private contactService: AddContactService,
+    private router: Router
+  ) {}
 
   myContacts: Contact[] = []
 
   ngOnInit(): void {
-    this.contact.getContacts().subscribe({
+    this.contactService.getContacts().subscribe({
       next: value => (this.myContacts = value),
       error: err => console.log(err),
+    })
+  }
+
+  editContact(contact: Contact) {
+    this.router.navigateByUrl(`/my-contacts/contact/${contact.id}/edit`, {
+      state: contact,
     })
   }
 }
