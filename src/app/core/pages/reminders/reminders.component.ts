f0269bc0174c684d4c7f50d1c407650fa568fd9e
Andrew Domingues <contato@drewdomi.com>
2024-07-04 16:29:53 -0300

    feat: create reminders template

--- src/app/core/pages/reminders/reminders.component.ts
+++ src/app/core/pages/reminders/reminders.component.ts
@@ -1,12 +1,24 @@
 import { CommonModule } from '@angular/common'
 import { Component } from '@angular/core'
-import { TitleComponent } from '../../components/title/title.component'
 
+import { AccordionModule } from 'primeng/accordion'
+import { MenuItem } from 'primeng/api'
+import { TabMenuModule } from 'primeng/tabmenu'
+
+import { TitleComponent } from '../../components/title/title.component'
 @Component({
   standalone: true,
   selector: 'gl-reminders',
   templateUrl: './reminders.component.html',
   styleUrl: './reminders.component.scss',
-  imports: [CommonModule, TitleComponent],
+  imports: [CommonModule, TitleComponent, TabMenuModule, AccordionModule],
 })
-export class RemindersComponent {}
+export class RemindersComponent {
+  items: MenuItem[] = [
+    { label: 'Hoje' },
+    { label: 'Agendados' },
+    { label: 'Todos' },
+  ]
+
+  activeItem = this.items[0]
+}
