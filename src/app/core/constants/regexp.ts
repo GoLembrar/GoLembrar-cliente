5d9f9c17ab9c8f70ff25271880cd4a4e9a644ae1
jottave <joao_victorflamengo10@outlook.com>
2024-08-16 18:12:41 -0300

    fix: refactor input data name, email and password

--- src/app/core/constants/regexp.ts
+++ src/app/core/constants/regexp.ts
@@ -1,2 +1,5 @@
-export const REGEX_PASSWORD = '^(?=.*?[A-Z])(?=.*?[a-z]).{6,}$'
-export const REGEX_PHONE = '([0-9]{2,3})?(\([0-9]{2}\))([0-9]{4,5})([0-9]{4})'
+export const REGEX_NAME = '^[A-Za-zÀ-ÖØ-ÿẀ-Ỳs]{3,100}$'
+export const REGEX_EMAIL =
+  '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$|^.{4,100}$'
+export const REGEX_PASSWORD = '^(?=.*?[A-Z])(?=.*?[a-z]).{6,25}$'
+export const REGEX_PHONE = '([0-9]{2,3})?(([0-9]{2}))([0-9]{4,5})([0-9]{4})'
