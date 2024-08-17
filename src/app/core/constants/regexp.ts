5d510033948763a2c355bd4169547c5f099c0e62
jottave <joao_victorflamengo10@outlook.com>
2024-08-16 23:15:49 -0300

    fix: refactor inputs name and identify

--- src/app/core/constants/regexp.ts
+++ src/app/core/constants/regexp.ts
@@ -1,6 +1,5 @@
 export const REGEX_NAME = '^[A-Za-zÀ-ÖØ-ÿẀ-Ỳs]{3,100}$'
-export const REGEX_EMAIL =
-  '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$|^.{4,100}$'
+export const REGEX_EMAIL = `[A-Za-z0-9\._%+\-]+@[A-Za-z0-9\.\-]+\.[A-Za-z]{2,}`
 export const REGEX_PASSWORD = '^(?=.*?[A-Z])(?=.*?[a-z]).{6,25}$'
 export const REGEX_PHONE = '([0-9]{2,3})?(([0-9]{2}))([0-9]{4,5})([0-9]{4})'
 export const REGEX_TITLE =
--- src/app/core/pages/contacts/add-contact/add-contact.component.ts
+++ src/app/core/pages/contacts/add-contact/add-contact.component.ts
@@ -1,24 +1,25 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'
+import { Component } from '@angular/core'
 import {
   FormBuilder,
   ReactiveFormsModule,
   Validators as V,
-} from '@angular/forms';
-import { MessageService } from 'primeng/api';
-import { AvatarModule } from 'primeng/avatar';
-import { ButtonModule } from 'primeng/button';
-import { DropdownModule } from 'primeng/dropdown';
-import { InputMaskModule } from 'primeng/inputmask';
-import { InputTextModule } from 'primeng/inputtext';
-import { TreeSelectModule } from 'primeng/treeselect';
-import { BackButtonComponent } from 'src/app/core/components/back-button/back-button.component';
-import { TitleComponent } from 'src/app/core/components/title/title.component';
-import { contactChannels } from 'src/app/core/constants/contact-channels';
-import { Contact } from 'src/app/core/models/contact';
-import { Channel } from 'src/app/core/models/enums/channels';
-import { ContactService } from 'src/app/core/services/contact/contact.service';
-import { getInputError, inputInvalid } from 'src/app/core/utils/input';
+} from '@angular/forms'
+import { MessageService } from 'primeng/api'
+import { AvatarModule } from 'primeng/avatar'
+import { ButtonModule } from 'primeng/button'
+import { DropdownModule } from 'primeng/dropdown'
+import { InputMaskModule } from 'primeng/inputmask'
+import { InputTextModule } from 'primeng/inputtext'
+import { TreeSelectModule } from 'primeng/treeselect'
+import { BackButtonComponent } from 'src/app/core/components/back-button/back-button.component'
+import { TitleComponent } from 'src/app/core/components/title/title.component'
+import { contactChannels } from 'src/app/core/constants/contact-channels'
+import { REGEX_EMAIL, REGEX_NAME } from 'src/app/core/constants/regexp'
+import { Contact } from 'src/app/core/models/contact'
+import { Channel } from 'src/app/core/models/enums/channels'
+import { ContactService } from 'src/app/core/services/contact/contact.service'
+import { getInputError, inputInvalid } from 'src/app/core/utils/input'
 
 @Component({
   selector: 'gl-profile',
@@ -48,9 +49,9 @@ export class AddContactComponent {
   ) {}
 
   protected newContact = this.formBuilder.group({
-    name: ['', [V.required, V.minLength(2), V.maxLength(60)]],
+    name: ['', [V.required, V.pattern(REGEX_NAME)]],
     channel: [Channel.EMAIL, [V.required]],
-    identify: ['', [V.required, V.email, V.minLength(2), V.maxLength(60)]],
+    identify: ['', [V.required, V.pattern(REGEX_EMAIL)]],
   })
 
   channels = contactChannels
